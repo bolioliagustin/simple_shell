fa87d8eef90a8fa3f13cf76576ccc695dd2d3b1c
Marcelo Arbiza <3338@holbertonschool.com>
2021-08-17 13:02:47 -0700

    178

---
+++ strtok.c
@@ -0,0 +1,30 @@
+#include "lib.h"
+int qStrtok(char *c)
+{
+ 	char *copy;
+	char *tok;
+       int i = 0, j = 0, k = 0;
+
+	for (; c[j]; j++)
+		;
+
+	copy = malloc(sizeof(char) * j);
+	
+	strcpy(copy, c);
+       if (c)
+       {
+       		tok = strtok(copy, " ");
+       		if (tok)
+       		{
+                	while (tok)
+                	{
+                        	i++; 
+				tok = strtok(NULL, " ");
+                	}
+       		}
+       }
+
+	free(copy);
+        return (i);
+}
+

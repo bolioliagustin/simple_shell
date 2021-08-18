fc2a63dc831a8444eea12f19c13f401fb28e293a
bolioliagustin <bolioliagustin@gmail.com>
2021-08-18 06:03:33 -0700

    s

---
+++ strncat.c
@@ -0,0 +1,15 @@
+#include "lib.h"
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i, j;
+
+for (i = 0; *(dest + i); i++)
+	;
+
+dest[i] = ' ';
+for (j = 0; j < n && src[j] != '\0'; j++)
+	dest[i + j] = src[j];
+dest[i + j] = '\0';
+return (dest);
+}
